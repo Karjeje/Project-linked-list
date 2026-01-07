086b6de4b78967d60bbfd2d87316a121562eb798
Karjeje <193631637+Karjeje@users.noreply.github.com>
2026-01-08 00:30:31 +0100

    Add append method

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -4,6 +4,20 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  append(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      return;
+    } else {
+      let current = this.head;
+      while (current.nextNode !== null) {
+        current = current.nextNode;
+      }
+      current.nextNode = newNode;
+    }
+  }
 }
 
 export default LinkedList;
