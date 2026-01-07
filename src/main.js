5f8d2c2be5cc5d106b3b95b6cabf54d7c3afabc5
Karjeje <193631637+Karjeje@users.noreply.github.com>
2026-01-08 00:39:07 +0100

    Add display helper function and test

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 import LinkedList from './LinkedList.js';
 
 const list = new LinkedList();
+
+list.append('dog');
+list.append('cat');
+list.prepend('bobakau');
+
+let current = list.head;
+while (current !== null) {
+  console.log(current.value);
+  current = current.nextNode;
+}
